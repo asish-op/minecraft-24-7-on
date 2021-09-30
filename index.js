9173768fd87c6c90119d7a0264ed346af1c404a1
asish <87756049+asish-op@users.noreply.github.com>
2021-09-30 12:21:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const {mineboty} = require("mineboty")
+
+ mineboty();  
+ //npm init -y && npm i --save-dev node@16.6.0 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
+ 
+ //to make v16 
